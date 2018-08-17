d336388ee482daac51a923af0b67f7f2de80f77d
kunal pal <imkunal.paul@gmail.com>
2018-08-17 06:34:08 +0530

    need to add progress bar and test it

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 const userSchema = new schema({
     name:{type:String,required:[true,"Please provied name"]},
-    songName:{type:String,required:[true,"Please selected a song"]},
+    songName:{type:String,required:[true,"Please provied song name"]},
     dedicatedTo:{type:String},
     userImage:{data: Buffer, contentType: String},
-    dedicatedToImage:{data: Buffer, contentType: String}
+    dedicatedToImage:{data: Buffer, contentType: String},
+    mp3:{data:Buffer,contentType:String}
 
 })
 const user = mongoose.model('userData',userSchema)
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const routes = express.Router()
 const user = require('../models/user')
+var fs = require("fs")
 var multer  = require('multer')
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploadImages')
+      var d = new Date()
+      var dir = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate()+'-'+d.getHours()+"-"+d.getMinutes()+"-"+d.getSeconds()+"-"+d.getMilliseconds();
+      /*fs.mkdirSync(`uploads/${dir}`,0o777).then((e)=>{
+        console.log("wat is this :----",e)
+      });*/
+      /*fs.exists(`uploads/${dir}`,(e) => {
+        //if(e){cb(null, `uploads/${dir}`)}
+        alert(e)
+      })*/
+      fs.mkdir(`uploads/${dir}`,(e) =>{
+        if(e){console.log("can't created a dir")}
+        else{cb(null, `uploads/${dir}`)}
+      })
+       
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now())
@@ -19,7 +33,6 @@ routes.get('/get',(req,res,next) => {
 })
 
 routes.post('/post',upload.any(),(req,res,next) => {
-    // Everything went fine
     user.create(req.body)
     .then((response) => {res.send({response})})
     .catch(next)
